34cbdb952d807041863e7c9a24538f5c31dc1307
daesung <daesung@archivsoft.com>
2020-01-09 15:55:05 +0900

    - Add to Spring Security - Add to Security login example - Add to HTML Publisher
     ※ Publish
     HTML5
     Framework - Boilerplate
     Not use to layout

---
+++ src/main/java/com/archivsoft/domain/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.archivsoft.domain.repository;
+
+import com.archivsoft.domain.entity.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByEmail(String userEmail);
+}
+
